04ff294ab8cce813fb622a85d106e2c9f64209ad
lucas phan <planh.qn@gmai.com>
2026-05-13 09:56:46 +0700

    Initial Commit

---
+++ TravelPinTests/TravelPinTests.swift
@@ -0,0 +1,19 @@
+//
+//  TravelPinTests.swift
+//  TravelPinTests
+//
+//  Created by longanh on 13/5/26.
+//
+
+import Testing
+@testable import TravelPin
+
+struct TravelPinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
